2c5b0a2e9171ff26d960602dfc5b6573d319fdce
lushusheng <lushushen8@163.com>
2019-10-09 17:44:09 +0800

    commit files

---
+++ bean/src/main/java/com/example/entity/carry/CommandParameter.java
@@ -0,0 +1,19 @@
+package com.example.entity.carry;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * 上行消息的通用实体头部
+ * @author lushusheng
+ * @date 2019-09-19
+ * @description
+ */
+@NoArgsConstructor
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class CommandParameter implements Serializable {
+}
---
+++ bean/src/main/java/com/example/entity/carry/NewProtocolDownloadProfileDataCommandParameter.java
@@ -0,0 +1,5 @@
+package com.example.entity.carry;
+
+
+public class NewProtocolDownloadProfileDataCommandParameter extends CommandParameter{
+}
