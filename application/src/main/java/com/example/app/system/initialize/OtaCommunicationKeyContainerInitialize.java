0a605595c40a8bfcf2487417a705b790aed76d44
lushusheng <lushushen8@163.com>
2019-10-11 18:20:51 +0800

    commit files

--- application/src/main/java/com/example/app/system/initialize/OtaCommunicationKeyContainerInitialize.java
+++ application/src/main/java/com/example/app/system/initialize/OtaCommunicationKeyContainerInitialize.java
@@ -1,7 +1,11 @@
 package com.example.app.system.initialize;
 
+import com.example.core.service.IManufacturerKeyService;
+import com.example.core.service.IMccBitMapService;
+import com.example.core.service.IPlmnIndexSerivce;
 import com.example.entity.container.OtaCommunicationKeyContainer;
 import com.example.entity.table.PlmnIndex;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.core.annotation.Order;
@@ -16,13 +20,39 @@ import java.util.Map;
  */
 @Component
 @Order(1)
+@RequiredArgsConstructor
 public class OtaCommunicationKeyContainerInitialize implements ApplicationRunner {
+
+    private final IManufacturerKeyService manufacturerKeyService;
+    private final IMccBitMapService mccBitMapService;
+    private final IPlmnIndexSerivce plmnIndexSerivce;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        Map<String, String[]> otaCommunicationKeysMap = OtaCommunicationKeyContainer.COMMUNICATION_KEY_MAP;
+
+        Map<String, String[]> otaCommunicationKeyMap = OtaCommunicationKeyContainer.COMMUNICATION_KEY_MAP;
+        loadOtaCommunicationKeyMap(otaCommunicationKeyMap);
+
         Map<String, String[]> mccBitMap = OtaCommunicationKeyContainer.MCC_BIT_MAP;
+        loadMccBitMap(mccBitMap);
+
         Map<String, PlmnIndex> plmnIndexMap = OtaCommunicationKeyContainer.PLMN_INDEX_MAP;
+        loadPlmnIndexMap(plmnIndexMap);
+
         Map<String, String[]> persDataKeyMap = OtaCommunicationKeyContainer.PERS_DATA_KEY_MAP;
+        loadPersDataKeyMap(persDataKeyMap);
+    }
+
+    private void loadOtaCommunicationKeyMap(Map<String, String[]> otaCommunicationKeyMap) {
+
+    }
+    private void loadMccBitMap(Map<String, String[]> mccBitMap) {
+
+    }
+    private void loadPlmnIndexMap(Map<String, PlmnIndex> plmnIndexMap) {
+
+    }
+    private void loadPersDataKeyMap(Map<String, String[]> persDataKeyMap) {
+
     }
 }
