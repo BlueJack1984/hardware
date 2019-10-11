0aaa08b821ed78a9fea55780888fdf7a1bba6813
lushusheng <lushushen8@163.com>
2019-10-11 10:01:34 +0800

    commit files

--- utils/src/main/java/com/example/util/protocol/ProtocolUtil.java
+++ utils/src/main/java/com/example/util/protocol/ProtocolUtil.java
@@ -42,56 +42,49 @@ public class ProtocolUtil {
             //0x2a表示字符*号
             if(0x2a == source[sourcePosition]) {
                 sourcePosition ++;
-                switch() {
+                switch(source[sourcePosition]) {
+                    //字符A
+                    case 0x30:
+                        destination[destinationPosition] = 0x41;
+                        break;
+                    //字符B
+                    case 0x31:
+                        destination[destinationPosition] = 0x42;
+                        break;
+                    //字符C
+                    case 0x32:
+                        destination[destinationPosition] = 0x43;
+                        break;
+                    //字符D
+                    case 0x33:
+                        destination[destinationPosition] = 0x44;
+                        break;
+                    //字符E
+                    case 0x34:
+                        destination[destinationPosition] = 0x45;
+                        break;
+                    //字符F
+                    case 0x35:
+                        destination[destinationPosition] = 0x46;
+                        break;
+                    //字符G
+                    case 0x36:
+                        destination[destinationPosition] = 0x47;
+                        break;
+                    //字符H
+                    case 0x37:
+                        destination[destinationPosition] = 0x48;
+                        break;
+                    //字符I
+                    case 0x38:
+                        destination[destinationPosition] = 0x49;
+                        break;
                 }
             }else {
                 destination[destinationPosition] = source[sourcePosition];
-                destinationPosition ++;
-                sourcePosition ++;
             }
+            destinationPosition ++;
+            sourcePosition ++;
         }while(sourcePosition <= sourceOffsetEnd);
-
-        short off_end = (short)(inoff+len),dst_start = outoff;
-        do{
-            if(src[inoff]==0x2A){
-                inoff++;
-                switch (src[(short)(inoff++)])
-                {
-                    case (byte) 0x30://A
-                        dst[outoff++] = 0x41;
-                        break;
-                    case (byte) 0x31://B
-                        dst[outoff++] = 0x42;
-                        break;
-                    case (byte) 0x32://C
-                        dst[outoff++] = 0x43;
-                        break;
-                    case (byte) 0x33://D
-                        dst[outoff++] = 0x44;
-                        break;
-                    case (byte) 0x34://E
-                        dst[outoff++] = 0x45;
-                        break;
-                    case (byte) 0x35://F
-                        dst[outoff++] = 0x46;
-                        break;
-                    case (byte) 0x36://
-                        dst[outoff++] = 0x47;
-                        break;
-                    case (byte) 0x37://H
-                        dst[outoff++] = 0x48;
-                        break;
-                    case (byte) 0x38://I
-                        dst[outoff++] = 0x49;
-                        break;
-                    default:
-                }
-
-            }else{
-                dst[outoff++] = src[(short)(inoff++)];
-            }
-        }while(inoff<=off_end);
-        return len = (short)(outoff - dst_start);
-    }
     }
 }
